33e190e63b069820499a6d947040e5c010f4e968
meox3259 <meox1223@gmail.com>
2025-03-26 22:27:25 +0900

    [bio] tmp modify

--- src/sequence_handler.cc
+++ src/sequence_handler.cc
@@ -8,9 +8,15 @@
 
 #include <algorithm>
 #include <assert.h>
+#include <cinttypes>
 #include <cmath>
 #include <cstring>
 #include <memory>
+#include <numeric>
+#include <string_view>
+#include <unordered_map>
+
+const int INF = std::numeric_limits<int>::max() / 2;
 
 int match = 1, mis = 2, gap_open = 2, gap_ext = 1;
 int8_t mat[25] = {1,  -2, -2, -2, 0,  -2, 1, -2, -2, 0, -2, -2, 1,
@@ -18,7 +24,11 @@ int8_t mat[25] = {1,  -2, -2, -2, 0,  -2, 1, -2, -2, 0, -2, -2, 1,
 
 namespace factor {
 
-int lower_bound = 1000, upper_bound = 3000;
+const double error_rate = 0.01;
+const int lower_bound = 1000, upper_bound = 3000;
+const int gap_delta_threshold = 50;
+
+const int min_anchor_size = 5, max_anchor_size = 20;
 
 bool is_gap_valid(int gap) { return gap >= lower_bound && gap <= upper_bound; }
 
@@ -38,17 +48,42 @@ bool calculate_sparsity(const std::vector<int> &index, int gap_threshold,
   }
 }
 
-std::vector<int> get_suspect_region(const std::vector<int> &index) {
-  int len = index.size();
-  for (int i = 0; i < len; ++i) {
-    std::vector<int> gap_size;
-    for (int j = 1; j < 10; ++j) {
-      gap_size.push_back(index[i + j] - index[i + j - 1]);
-    }
-    for (int gap : gap_size) {
-      if
+bool check_gap_set_is_valid(const std::vector<int> &index) {
+  std::vector<int> gap_set;
+  for (int i = 0; i < static_cast<int>(index.size()) - 1; ++i) {
+    gap_set.push_back(index[i + 1] - index[i]);
+  }
+  int len = gap_set.size();
+  int avg = accumulate(gap_set.begin(), gap_set.end(), 0) / len;
+  if (!is_gap_valid(avg)) {
+    return false;
+  }
+  for (int gap : gap_set) {
+    if (!is_gap_valid(gap) || std::abs(gap - avg) > gap_delta_threshold) {
+      return false;
     }
   }
+  return true;
+}
+
+std::vector<std::pair<int, std::vector<int>>>
+collect_suspect_region(const std::vector<int> &index) {
+  std::vector<std::pair<int, std::vector<int>>> suspect_region;
+  int len = index.size();
+  for (int i = 0; i < len; ++i) {
+    for (int j = max_anchor_size; j >= min_anchor_size; --j) {
+      std::vector<int> anchor_index;
+      for (int k = 0; k <= j; ++k) {
+        anchor_index.push_back(index[i + k]);
+      }
+      if (check_gap_set_is_valid(anchor_index)) {
+        suspect_region.emplace_back((index[i + j] - index[i]) / (j - 1),
+                                    std::move(anchor_index));
+        break;
+      }
+    }
+  }
+  return suspect_region;
 }
 
 bool get_overlap(const std::vector<int> &l, const std::vector<int> &r,
@@ -58,17 +93,209 @@ bool get_overlap(const std::vector<int> &l, const std::vector<int> &r,
     return false;
   }
   int l0 = l[0], l1 = l.back(), r0 = r[0], r1 = r.back();
-  int l_cross =
-      upper_bound(r.begin(), r.end(), l1) - lower_bound(r.begin(), r.end(), l0);
-  int r_cross =
-      upper_bound(l.begin(), l.end(), r1) - lower_bound(l.begin(), l.end(), r0);
+  int l_cross = std::upper_bound(r.begin(), r.end(), l1) -
+                std::lower_bound(r.begin(), r.end(), l0);
+  int r_cross = std::upper_bound(l.begin(), l.end(), r1) -
+                std::lower_bound(l.begin(), l.end(), r0);
   int l_size = l.size(), r_size = r.size();
   double overlap_l = (double)l_cross / (double)(l_size);
   double overlap_r = (double)r_cross / (double)(r_size);
   return overlap_l >= overlap_threshold && overlap_r >= overlap_threshold;
 }
 
+std::vector<int> deal_with_gap_values(std::vector<int> &gap_values) {
+  std::sort(gap_values.begin(), gap_values.end());
+  int len = gap_values.size();
+  std::vector<int> partition_index{-1};
+  for (int i = 0; i < len - 1; ++i) {
+    if (gap_values[i + 1] - gap_values[i] > gap_delta_threshold) {
+      partition_index.push_back(i);
+    }
+  }
+  partition_index.push_back(len);
+  std::vector<int> result;
+  int partition_index_size = partition_index.size();
+  for (int i = 0; i < partition_index_size - 1; ++i) {
+    int start = partition_index[i] + 1;
+    int end = partition_index[i + 1];
+    int avg =
+        accumulate(gap_values.begin() + start, gap_values.begin() + end, 0) /
+        (end - start);
+    result.push_back(avg);
+  }
+  return result;
+}
+
 } // namespace factor
+
+namespace alignment {
+
+std::vector<int> ksw2_get_xid(uint32_t *cigar, int n_cigar,
+                              const uint8_t *query, const uint8_t *target) {
+  int qi = 0;
+  int ti = 0;
+  std::vector<int> xid(4);
+  for (int i = 0; i < n_cigar; ++i) {
+    int op = cigar[i] & 0xf;
+    int len = cigar[i] >> 4;
+    if (op == 0) {
+      // match/mismatch
+      for (int j = 0; j < len; ++j) {
+        if (query[qi + j] == target[ti + j]) {
+          xid[0]++;
+        } else {
+          xid[3]++;
+        }
+      }
+      qi += len;
+      ti += len;
+    } else if (op == 1) {
+      xid[1] += len;
+      qi += len;
+    } else if (op == 2) {
+      xid[2] += len;
+      ti += len;
+    } else {
+      return {};
+    }
+  }
+  return xid;
+}
+
+std::pair<int, int> alignment(uint8_t *query, int qlen, uint8_t *target,
+                              int tlen) {
+  ksw_extz_t ez;
+  memset(&ez, 0, sizeof(ksw_extz_t));
+  int w = -1, zdrop = -1, end_bonus = 0, flag = 0;
+
+  ksw_extz2_sse(0, qlen, query, tlen, target, 5, mat, gap_open, gap_ext, w,
+                zdrop, end_bonus, flag, &ez);
+  int *n_cigar;
+  *n_cigar = ez.n_cigar;
+  uint32_t **cigar;
+  *cigar = ez.cigar;
+  std::vector<int> xid = ksw2_get_xid(*cigar, *n_cigar, query, target);
+  free(cigar);
+  return {xid[0], accumulate(xid.begin(), xid.end(), 0)};
+}
+
+std::pair<int, int> alignment(const std::string &query_string,
+                              const std::string &target_string) {
+  ksw_extz_t ez;
+  memset(&ez, 0, sizeof(ksw_extz_t));
+  int w = -1, zdrop = -1, end_bonus = 0, flag = 0;
+
+  uint8_t *query = alloc_uint8_t(query_string);
+  uint8_t *target = alloc_uint8_t(target_string);
+  int qlen = static_cast<int>(query_string.size());
+  int tlen = static_cast<int>(target_string.size());
+  ksw_extz2_sse(0, qlen, query, tlen, target, 5, mat, gap_open, gap_ext, w,
+                zdrop, end_bonus, flag, &ez);
+  int *n_cigar;
+  *n_cigar = ez.n_cigar;
+  uint32_t *cigar;
+  cigar = ez.cigar;
+
+  std::vector<int> xid = ksw2_get_xid(cigar, *n_cigar, query, target);
+  free(query);
+  free(target);
+  free(cigar);
+  return {xid[0], accumulate(xid.begin(), xid.end(), 0)};
+}
+
+int ksw2_backtrack_left_end(int n_cigar, uint32_t *cigar, int qlen, int tlen,
+                            int q_left_ext) {
+  int t_left_ext = 0, i, j, op, len;
+  int q_remain_len = q_left_ext;
+  for (i = n_cigar - 1; i >= 0; --i) {
+    op = cigar[i] & 0xf;
+    len = cigar[i] >> 4;
+    if (op == 0) { // MATCH/MISMATCH
+      if (len >= q_remain_len) {
+        t_left_ext += q_remain_len;
+        return t_left_ext;
+      } else {
+        t_left_ext += len;
+        q_remain_len -= len;
+      }
+    } else if (op == 1) { // INSERTION
+      if (len >= q_remain_len) {
+        return t_left_ext;
+      } else {
+        q_remain_len -= len;
+      }
+    } else if (op == 2) { // DELETION
+      t_left_ext += len;
+    }
+  }
+  if (q_remain_len > 0) {
+    std::cout << "Error: unmatched cigar and q_left_ext." << std::endl;
+  }
+  return t_left_ext;
+}
+
+int ksw2_backtrack_right_end(int n_cigar, uint32_t *cigar, int qlen, int tlen,
+                             int q_right_ext) {
+  int t_right_ext = 0, i, j, op, len;
+  int q_remain_len = q_right_ext;
+  for (i = 0; i < n_cigar; ++i) {
+    op = cigar[i] & 0xf;
+    len = cigar[i] >> 4;
+    if (op == 0) { // MATCH/MISMATCH
+      if (len >= q_remain_len) {
+        t_right_ext += q_remain_len;
+        return t_right_ext;
+      } else {
+        t_right_ext += len;
+        q_remain_len -= len;
+      }
+    } else if (op == 1) { // INSERTION
+      if (len >= q_remain_len) {
+        return t_right_ext;
+      } else {
+        q_remain_len -= len;
+      }
+    } else if (op == 2) { // DELETION
+      t_right_ext += len;
+    }
+  }
+  if (q_remain_len > 0) {
+    std::cout << "Error: unmatched cigar and q_right_ext." << std::endl;
+  }
+  return t_right_ext;
+}
+
+int extend_left_boundary(uint8_t *query, int qlen, uint8_t *target, int tlen) {
+  ksw_extz_t ez;
+  memset(&ez, 0, sizeof(ksw_extz_t));
+  int w = -1, zdrop = -1, end_bonus = 0, flag = 0;
+
+  ksw_extz2_sse(0, qlen, query, tlen, target, 5, mat, gap_open, gap_ext, w,
+                zdrop, end_bonus, flag, &ez);
+  int *n_cigar;
+  *n_cigar = ez.n_cigar;
+  uint32_t *cigar;
+  cigar = ez.cigar;
+  std::vector<int> xid = ksw2_get_xid(cigar, *n_cigar, query, target);
+  return ksw2_backtrack_left_end(*n_cigar, cigar, qlen, tlen, xid[0]);
+}
+
+int extend_right_boundary(uint8_t *query, int qlen, uint8_t *target, int tlen) {
+  ksw_extz_t ez;
+  memset(&ez, 0, sizeof(ksw_extz_t));
+  int w = -1, zdrop = -1, end_bonus = 0, flag = 0;
+
+  ksw_extz2_sse(0, qlen, query, tlen, target, 5, mat, gap_open, gap_ext, w,
+                zdrop, end_bonus, flag, &ez);
+  int *n_cigar;
+  *n_cigar = ez.n_cigar;
+  uint32_t *cigar;
+  cigar = ez.cigar;
+  std::vector<int> xid = ksw2_get_xid(cigar, *n_cigar, query, target);
+  return ksw2_backtrack_right_end(*n_cigar, cigar, qlen, tlen, xid[0]);
+}
+
+} // namespace alignment
 
 void parse_index_set(const std::vector<int> &index) {
   int len = index.size();
@@ -91,86 +318,172 @@ void solve(uint8_t *s, int len, const Param &opt) {
 
   std::cerr << "start sam calc" << std::endl;
   std::vector<std::vector<int>> index_set;
-  for (int i = 0; i < sam->size(); ++i) {
-    auto right_index_set = sam->get_right_index_by_node(i);
-    if (right_index_set.size() < 4) {
-      continue;
-    }
-  }
+
+  std::vector<std::pair<int, std::vector<int>>> suspect_region;
+  std::vector<int> gap_values;
   for (int i = 0; i < sam->size(); ++i) {
     auto right_index_i = sam->get_right_index_by_node(i);
-    if (right_index_i.size() < 4) {
+    if (right_index_i.size() < factor::min_anchor_size) {
       continue;
     }
-    if (calculate_sparsity(right_index_i, opt.gap_threshold, opt.gap_ratio)) {
-      index_set.push_back(std::move(right_index_i));
+    auto current_suspect_region = factor::collect_suspect_region(right_index_i);
+    for (const auto &[gap, anchor_index] : current_suspect_region) {
+      suspect_region.emplace_back(gap, std::move(anchor_index));
+      gap_values.push_back(gap);
     }
   }
 
-  sort(index_set.begin(), index_set.end(),
-       [](const auto &lhs, const auto &rhs) {
-         return *lhs.begin() < *rhs.begin();
-       });
+  std::vector<std::vector<std::vector<int>>> bucket_of_index_partition(
+      gap_values.size());
+  std::vector<std::vector<int>> bucket_of_index(gap_values.size());
 
-  auto merge = [](std::vector<int> &lhs, std::vector<int> &rhs) {
-    if (lhs.size() > rhs.size()) {
-      swap(lhs, rhs);
+  std::sort(
+      suspect_region.begin(), suspect_region.end(),
+      [](const auto &lhs, const auto &rhs) { return lhs.first < rhs.first; });
+
+  int suspect_region_size = suspect_region.size();
+  for (int i = 0; i < suspect_region_size; ++i) {
+    const auto &[gap, anchor_index] = suspect_region[i];
+    int gap_index =
+        std::lower_bound(gap_values.begin(), gap_values.end(), gap) -
+        gap_values.begin();
+    int dis1 = gap_index > 0 ? std::abs(gap - gap_values[gap_index - 1]) : INF;
+    int dis2 = gap_index < gap_values.size() - 1
+                   ? std::abs(gap - gap_values[gap_index + 1])
+                   : INF;
+    if (dis1 < dis2) {
+      bucket_of_index_partition[gap_index - 1].push_back(
+          std::move(anchor_index));
+      for (int index : anchor_index) {
+        bucket_of_index[gap_index - 1].push_back(index);
+      }
+    } else {
+      bucket_of_index_partition[gap_index].push_back(std::move(anchor_index));
+      for (int index : anchor_index) {
+        bucket_of_index[gap_index].push_back(index);
+      }
     }
-    for (int i : lhs) {
-      rhs.emplace_back(i);
+  }
+
+  for (auto &vec : bucket_of_index) {
+    std::sort(vec.begin(), vec.end(),
+              [](const auto &lhs, const auto &rhs) { return lhs < rhs; });
+  }
+
+  // 暂时不需要用，用于确定真正的区间大小
+  for (auto &vec : bucket_of_index_partition) {
+    std::sort(vec.begin(), vec.end(),
+              [](const auto &lhs, const auto &rhs) { return lhs[0] < rhs[0]; });
+  }
+
+  std::vector<std::pair<int, int>> raw_estimate_unit_region;
+
+  // 排好序后，枚举每个端点求一个区间和
+  for (int i = 0; i < gap_values.size(); ++i) {
+    int unit_size = gap_values[i];
+    int k = 0;
+    int len = bucket_of_index[i].size();
+    for (int j = 0; j < len; ++j) {
+      int start_position = bucket_of_index[i][j];
+      int end_position = start_position + unit_size;
+      while (k < len && bucket_of_index[i][k] < end_position) {
+        k++;
+      }
+      int num_of_index = k - j;
+
+      // 如果区间内点数>= unit_size * error_rate
+      if (num_of_index >= (int)(unit_size * factor::error_rate)) {
+        raw_estimate_unit_region.emplace_back(start_position, unit_size);
+      }
     }
-    std::sort(rhs.begin(), rhs.end());
-    lhs.clear();
+  }
+
+  std::sort(
+      raw_estimate_unit_region.begin(), raw_estimate_unit_region.end(),
+      [](const auto &lhs, const auto &rhs) { return lhs.first < rhs.first; });
+
+  int max_r_boundary = -1;
+  int max_l_boundary = -1;
+
+  std::unordered_map<int, std::pair<int, int>> max_covered_region;
+
+  auto already_covered = [&max_l_boundary, &max_r_boundary](int start_position,
+                                                            int end_position,
+                                                            int unit_size) {
+    if (end_position <= max_r_boundary && start_position >= max_l_boundary) {
+      if (end_position > max_r_boundary) {
+        max_r_boundary = end_position;
+        max_l_boundary = start_position;
+      }
+      return true;
+    }
+    if (end_position > max_r_boundary) {
+      max_r_boundary = end_position;
+      max_l_boundary = start_position;
+    }
+    return false;
   };
 
-  std::cerr << "set size = " << index_set.size() << std::endl;
-  std::cerr << "start overlap" << std::endl;
-  for (int i = 0; i < index_set.size(); ++i) {
-  }
-  while (true) {
-    int sz = index_set.size();
-    int merge_count = 0;
-    for (int i = 0; i < sz - 1; ++i) {
-      if (get_overlap(index_set[i], index_set[i + 1], opt.overlap_threshold)) {
-        merge(index_set[i], index_set[i + 1]);
-        merge_count++;
+  std::vector<std::tuple<int, int, int>> raw_estimate_interval;
+  for (const auto &[start_position, unit_size] : raw_estimate_unit_region) {
+    int end_position = start_position + unit_size;
+    if (already_covered(start_position, end_position, unit_size)) {
+      continue;
+    }
+    // 向左延伸
+    int raw_left_boundary = start_position;
+    int raw_right_boundary = end_position;
+    {
+      for (int i = start_position; i >= unit_size; i -= unit_size) {
+        // 做一下alignment，如果相似度太小就break
+        auto [match, length] = alignment::alignment(
+            s + i - unit_size, unit_size, s + start_position, unit_size);
+        if (1. - ((double)(match) / (double)(length)) <= factor::error_rate) {
+          break;
+        }
+        raw_left_boundary = i - unit_size;
       }
     }
-    if (merge_count == 0) {
-      break;
-    }
-    std::vector<std::vector<int>> new_index_set;
-    for (int i = 0; i < sz; ++i) {
-      if (!index_set[i].empty()) {
-        new_index_set.emplace_back(index_set[i]);
+    // 向右延伸
+    {
+      for (int i = end_position; i + unit_size <= len; i += unit_size) {
+        // 做一下alignment，如果相似度太小就break
+        auto [match, length] = alignment::alignment(
+            s + i, unit_size, s + start_position, unit_size);
+        if (1. - ((double)match / (double)(length)) <= factor::error_rate) {
+          break;
+        }
+        raw_right_boundary = i + unit_size;
       }
     }
-    swap(index_set, new_index_set);
+    if (max_covered_region.find(start_position) == max_covered_region.end() ||
+        max_covered_region[start_position].second < raw_right_boundary) {
+      max_covered_region[start_position] = {raw_left_boundary,
+                                            raw_right_boundary};
+    }
+    raw_estimate_interval.emplace_back(unit_size, raw_left_boundary,
+                                       raw_right_boundary);
   }
 
-  std::cerr << "size = " << index_set.size() << std::endl;
-  for (const auto &vec : index_set) {
-    std::cerr << "l = " << vec[0] << " " << "r = " << vec.back() << " "
-              << "size = " << vec.size() << std::endl;
-  }
   auto alignment_engine = spoa::AlignmentEngine::Create(
       spoa::AlignmentType::kNW, 3, -5, -3); // linear gaps
   spoa::Graph graph{};
 
-  for (const auto &vec : index_set) {
-    std::cerr << "l = " << vec[0] << " " << "r = " << vec.back() << " "
-              << "size = " << vec.size() << std::endl;
-    int seg_count = vec.size();
+  for (auto [unit_size, left_boundary, right_boundary] :
+       raw_estimate_interval) {
+    std::cerr << "l = " << left_boundary << " " << "r = " << right_boundary
+              << " " << "size = " << unit_size << std::endl;
+    int seg_count =
+        (right_boundary - left_boundary + unit_size - 1) / unit_size;
 
     std::vector<std::string> strings(seg_count);
-    for (int i = 0; i < seg_count - 1; ++i) {
+    for (int i = left_boundary; i + unit_size - 1 <= right_boundary;
+         i += unit_size) {
       std::string sequence("");
-      for (int j = vec[i]; j < vec[i + 1]; ++j) {
+      for (int j = i; j < i + unit_size; ++j) {
         sequence += safeNumberToDnaChar(s[j]);
       }
       strings.emplace_back(std::move(sequence));
-      std::cerr << "vec[i] = " << vec[i] << " vec[i + 1] = " << vec[i + 1];
-      std::cerr << " i = " << i << " sequence = " << sequence << std::endl;
     }
 
     for (auto &sequence : strings) {
@@ -181,23 +494,28 @@ void solve(uint8_t *s, int len, const Param &opt) {
     auto consensus = graph.GenerateConsensus();
     int consensus_len = static_cast<int>(consensus.size());
 
-    int total_length = vec.back() - vec[0] + 1;
-    int cur_seg = vec.size();
-    int unit_length = (total_length + cur_seg - 1) / cur_seg;
+    uint8_t *cons = alloc_uint8_t(consensus);
+    // 向左拓展边界
+    int left_ext = alignment::extend_left_boundary(
+        s + std::max(0, left_boundary - unit_size),
+        std::min(unit_size, left_boundary), cons, consensus_len);
 
-    ksw_extz_t ez;
-    memset(&ez, 0, sizeof(ksw_extz_t));
-    int w = -1, zdrop = -1, end_bonus = 0, flag = 0;
-
-    uint8_t *target = alloc_uint8_t(consensus);
-    ksw_extz2_sse(0, std::min(consensus_len, vec[0]),
-                  s + std::max(vec[0] - consensus_len, 0), consensus_len,
-                  target, 5, mat, gap_open, gap_ext, w, zdrop, end_bonus, flag,
-                  &ez);
-
-    int *n_cigar;
-    *n_cigar = ez.n_cigar;
-    uint32_t **cigar;
-    *cigar = ez.cigar;
+    // 向右拓展边界
+    int right_ext = alignment::extend_right_boundary(
+        s + std::min(len, right_boundary + unit_size),
+        std::min(unit_size, len - right_boundary), cons, consensus_len);
+    left_boundary -= left_ext;
+    right_boundary += right_ext;
+    double all_match = 0;
+    double all_total = 0;
+    for (int i = left_boundary; i < right_boundary; i += unit_size) {
+      auto [match, total] =
+          alignment::alignment(s + i, unit_size, cons, consensus_len);
+      all_match += match;
+      all_total += total;
+    }
+    std::cout << "[" << left_boundary << "," << right_boundary << "]" << " "
+              << all_match / all_total << ' ' << consensus << std::endl;
+    free(cons);
   }
 }
